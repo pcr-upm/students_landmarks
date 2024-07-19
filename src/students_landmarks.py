c374edb372f9950117b8d18e36a3988d80e8fef3
Roberto Valle <roberto.valle@upm.es>
2024-07-19 12:29:20 +0200

    Added DAD-3DHeads data set

--- src/students_landmarks.py
+++ src/students_landmarks.py
@@ -72,6 +72,8 @@ class StudentsLandmarks(Alignment):
             self.indices = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 24, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 1, 134, 2, 136, 3, 138, 139, 140, 141, 4, 143, 5, 145, 6, 147, 148, 149, 150, 151, 152, 153, 17, 16, 156, 157, 158, 18, 7, 161, 9, 163, 8, 165, 10, 167, 11, 169, 13, 171, 12, 173, 14, 175, 20, 177, 178, 22, 180, 181, 21, 183, 184, 23, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197]
         elif self.database in 'face_synthetics':
             self.indices = [101, 102, 103, 104, 105, 106, 107, 108, 24, 110, 111, 112, 113, 114, 115, 116, 117, 1, 119, 2, 121, 3, 4, 124, 5, 126, 6, 128, 129, 130, 17, 16, 133, 134, 135, 18, 7, 138, 139, 8, 141, 142, 11, 144, 145, 12, 147, 148, 20, 150, 151, 22, 153, 154, 21, 156, 157, 23, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170]
+        elif self.database in 'dad':
+            self.indices = [1983, 2189, 3708, 336, 335, 3153, 3705, 2178, 3684, 3741, 3148, 3696, 2585, 2565, 2567, 3764, 570, 694, 3865, 17, 16, 2134, 3863, 673, 3851, 3880, 2121, 3859, 1448, 1428, 1430, 3893, 2441, 2446, 2382, 2381, 2383, 2496, 3690, 2493, 2491, 2465, 3619, 3632, 2505, 2273, 2276, 2355, 2295, 2359, 2267, 2271, 2403, 2437, 1183, 1194, 1033, 1023, 1034, 1345, 3856, 1342, 1340, 1243, 3827, 3833, 1354, 824, 827, 991, 883, 995, 814, 822, 1096, 1175, 3540, 3704, 3555, 3560, 3561, 3501, 3526, 3563, 2793, 2751, 3092, 3099, 3102, 2205, 2193, 2973, 2868, 2921, 2920, 1676, 1623, 2057, 2064, 2067, 723, 702, 1895, 1757, 1818, 1817, 3515, 3541, 2828, 2832, 2833, 2850, 2813, 2811, 2774, 3546, 1657, 1694, 1696, 1735, 1716, 1715, 1711, 1719, 1748, 1740, 1667, 1668, 3533, 2785, 2784, 2855, 2863, 2836, 2891, 2890, 2892, 2928, 2937, 3509, 1848, 1826, 1789, 1787, 1788, 1579, 1773, 1774, 1795, 1802, 1865, 3503, 2948, 2905, 2898, 2881, 2880, 2715, 3386, 3381, 1962, 2213, 2259, 2257, 2954, 3171, 2003, 3554, 576, 2159, 1872, 798, 802, 731, 567, 3577, 3582, 3390, 3391, 3396, 3400, 3599, 3593, 3588, 3068, 2196, 2091, 3524, 628, 705, 2030]
         else:
             raise ValueError('Database is not implemented')
 
@@ -100,33 +102,33 @@ class StudentsLandmarks(Alignment):
         import torchsummary
         from images_framework.src.constants import Modes
         from images_framework.alignment.students_landmarks.src.lit_resnet import LitResNet
-        # from images_framework.alignment.students_landmarks.src.lit_shg import LitSHG
+        from images_framework.alignment.students_landmarks.src.lit_shg import LitSHG
         # Set up the neural network to train
         print('Load model')
         torch.set_float32_matmul_precision('medium')
         if self.backbone is Backbone.RESNET:
             self.model = LitResNet(num_classes=len(self.indices), version=self.version, lr=1e-3, patience=self.patience, batch_size=self.batch_size, transfer=True, tune_fc_only=False)
-        # elif self.backbone is Backbone.SHG:
-        #     self.model = LitSHG(num_classes=len(self.indices), version=self.version, lr=1e-3, patience=self.patience, batch_size=self.batch_size, transfer=True, tune_fc_only=False)
+        elif self.backbone is Backbone.SHG:
+            self.model = LitSHG(num_classes=len(self.indices), version=self.version, lr=1e-3, patience=self.patience, batch_size=self.batch_size, transfer=True, tune_fc_only=False)
         else:
             raise ValueError('Backbone is not implemented')
-        torchsummary.summary(self.model, input_size=(3, self.width, self.height), batch_size=self.batch_size, device='cpu')
+        self.model.to(self.device)
+        torchsummary.summary(self.model, input_size=(3, self.width, self.height), batch_size=self.batch_size, device=self.device.type)
         # Set up the neural network to test
         if mode is Modes.TEST:
             model_path = self.path + 'data/' + self.database + '/' + self.backbone.value + '/'
             print('Loading model from {}'.format(model_path))
             if self.backbone is Backbone.RESNET:
                 self.model = LitResNet.load_from_checkpoint(os.path.join(model_path+'ckpt/', 'best.ckpt'), num_classes=len(self.indices), version=self.version)
-            # elif self.backbone is Backbone.SHG:
-            #     self.model = LitSHG.load_from_checkpoint(os.path.join(model_path+'ckpt/', 'best.ckpt'), num_classes=len(self.indices), version=self.version)
-            self.model.to(self.device)
+            elif self.backbone is Backbone.SHG:
+                self.model = LitSHG.load_from_checkpoint(os.path.join(model_path+'ckpt/', 'best.ckpt'), num_classes=len(self.indices), version=self.version)
             self.model.eval()
 
     def process(self, ann, pred):
         from images_framework.src.datasets import Database
         from images_framework.src.annotations import GenericLandmark
         from images_framework.alignment.landmarks import lps
-        # from images_framework.alignment.students_landmarks.src.utils import get_landmarks_local_softmax
+        from images_framework.alignment.students_landmarks.src.utils import get_landmarks_local_softmax
         datasets = [subclass().get_names() for subclass in Database.__subclasses__()]
         idx = [datasets.index(subset) for subset in datasets if self.database in subset]
         parts = Database.__subclasses__()[idx[0]]().get_landmarks()
@@ -139,13 +141,13 @@ class StudentsLandmarks(Alignment):
                 outputs = self.model(batch['img'].float().to(self.device))
                 outputs = outputs.view(-1, len(self.indices), 2)
                 landmarks = outputs.squeeze().cpu().numpy()
-                # if self.backbone is Backbone.SHG:
-                #     output = get_landmarks_local_softmax(output, temperature=10, window=5, device=self.device).squeeze().cpu()
-                #     bbox_res = batch['bbox_res'][0]
-                #     bbox = batch['bbox'][0]
-                #     landmarks = output / 0.5
-                #     landmarks = (landmarks - bbox_res[0:2]) / bbox_res[2:4]
-                #     landmarks = (landmarks * bbox[2:4]) + bbox[0:2]
+                if self.backbone is Backbone.SHG:
+                    output = get_landmarks_local_softmax(output, temperature=10, window=5, device=self.device).squeeze().cpu()
+                    bbox_res = batch['bbox_res'][0]
+                    bbox = batch['bbox'][0]
+                    landmarks = output / 0.5
+                    landmarks = (landmarks - bbox_res[0:2]) / bbox_res[2:4]
+                    landmarks = (landmarks * bbox[2:4]) + bbox[0:2]
                 # Save prediction
                 obj_pred = pred.images[batch['idx_img']].objects[batch['idx_obj']]
                 bbox_enlarged = batch['bbox_enlarged'].squeeze().cpu().numpy()
