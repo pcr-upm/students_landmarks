eae84b58a8745de695c931db349c708ddfb355ec
Roberto Valle <roberto.valle@upm.es>
2026-09-14 18:40:35 +0200

    Use SSH forwarding for secure private repository cloning

--- test_students_landmarks_example.sh
+++ test_students_landmarks_example.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo 'Using Docker to start the container and run tests ...'
-sudo docker build --force-rm --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -t students_landmarks_image .
-sudo docker volume create --name students_landmarks_volume
-sudo docker run --name students_landmarks_container -v students_landmarks_volume:/home/username/students_landmarks --rm --gpus all -it -d students_landmarks_image bash
-sudo docker exec -w /home/username/students_landmarks students_landmarks_container python test/students_landmarks_test.py --input-data test/example.tif --database wflw --gpu 0 --regressor encoder --backbone resnet50 --save-image
-sudo docker stop students_landmarks_container
+sudo docker build --force-rm --ssh default=$HOME/.ssh/id_rsa -t students_landmarks_image .
+sudo docker run --name students_landmarks_container --rm --gpus all -it -d students_landmarks_image bash
+sudo docker exec -w /home/username/students_landmarks students_landmarks_container python test/students_landmarks_test.py --input-data test/example.tif --database 300wlp --gpu 0 --regressor encoder --backbone resnet50 --save-image
 echo 'Transferring data from docker container to your local machine ...'
 mkdir -p output
-sudo chown -R "${USER}":"${USER}" /var/lib/docker/
-rsync --delete -azvv /var/lib/docker/volumes/students_landmarks_volume/_data/conda/envs/students/lib/python3.10/site-packages/images_framework/output/images/ output
-sudo docker system prune --all --force --volumes
-sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+sudo docker cp students_landmarks_container:/home/username/conda/envs/students/lib/python3.10/site-packages/images_framework/output/images/. output/
+sudo chown -R "${USER}":"${USER}" output
+sudo docker rm -f students_landmarks_container
+sudo docker image rm students_landmarks_image
+sudo docker builder prune -a -f
